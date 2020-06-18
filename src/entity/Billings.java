9ad2283831a046b63ecff637ddb58b1cde60543c
dlutrick <davidrlutrick@gmail.com>
2020-06-17 21:47:51 -0700

    added rewards and billing entity files

--- src/entity/Billings.java
+++ src/entity/Billings.java
@@ -2,4 +2,58 @@ package entity;
 
 public class Billings {
 
+	private int id;
+	private int customerId;
+	private String state;
+	private String street;
+	private String zipcode;
+	
+	private Billings(int id, int customerId, String state, String street, String zipcode) {
+		this.id = id;
+		this.customerId = customerId;
+		this.state = state;
+		this.street = street;
+		this.zipcode = zipcode;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	
 }
--- src/entity/Rewards.java
+++ src/entity/Rewards.java
@@ -1,5 +1,57 @@
 package entity;
 
 public class Rewards {
+	private int id;
+	private int customerId;
+	private int requiredPoints;
+	private String rewardDescription;
+	private int discount;
+	
+	private Rewards(int id, int customerId, int requiredPoints, String rewardDescription, int discount) {
+		this.id = id;
+		this.customerId = customerId;
+		this.requiredPoints = requiredPoints;
+		this.rewardDescription = rewardDescription;
+		this.discount = discount;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public int getRequiredPoints() {
+		return requiredPoints;
+	}
+
+	public void setRequiredPoints(int requiredPoints) {
+		this.requiredPoints = requiredPoints;
+	}
+
+	public String getRewardDescription() {
+		return rewardDescription;
+	}
+
+	public void setRewardDescription(String rewardDescription) {
+		this.rewardDescription = rewardDescription;
+	}
+
+	public int getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
 }
