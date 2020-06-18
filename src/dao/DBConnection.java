d76e83bb9191bd90ee07fc30f392f0772c5831fb
dlutrick <davidrlutrick@gmail.com>
2020-06-17 21:09:56 -0700

    added DBConnection

--- src/dao/DBConnection.java
+++ src/dao/DBConnection.java
@@ -1,5 +1,34 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+	
+	private final static String URL = "jdbc:mysql://localhost:3306/hotelsDB";
+	private final static String USERNAME = "root";
+	private final static String PASSWORD = "root";
+	private static Connection connection;
+	private static DBConnection instance;
+	
+	private DBConnection(Connection connection) {
+		this.connection = connection;
+	}
+	
+	public static Connection getConnection() {
+		if(instance == null) {
+			try {
+				connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+				instance = new DBConnection(connection);
+				System.out.println("Connection Successful!");
+			} catch(SQLException e) {
+				e.getStackTrace();
+			}
+			
+		}
+		
+		return DBConnection.connection;
+	}
 
 }
