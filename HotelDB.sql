23c6656fcc54284b535361f4c93d4e7198881f71
dlutrick <davidrlutrick@gmail.com>
2020-06-20 22:59:47 -0700

    Update HotelDB.sql

--- HotelDB.sql
+++ HotelDB.sql
@@ -9,22 +9,22 @@ DROP TABLE IF EXISTS Customers;
 
 CREATE TABLE Customers (
 	id INT(11) NOT NULL AUTO_INCREMENT,
-	first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
-    email VARCHAR(320) NOT NULL,
-    phone_number VARCHAR(10) NOT NULL,
-    points_held CHAR(8),
-    PRIMARY KEY (id)
+    	first_name VARCHAR(20) NOT NULL,
+    	last_name VARCHAR(20) NOT NULL,
+    	email VARCHAR(320) NOT NULL,
+    	phone_number VARCHAR(10) NOT NULL,
+    	points_held CHAR(8),
+    	PRIMARY KEY (id)
 );
 
 CREATE TABLE Rewards (
-    id INT(11) NOT NULL AUTO_INCREMENT,
-    customer_id INT(11) NOT NULL,
-    required_points INT(11) NOT NULL,
-    reward_description VARCHAR(255) NOT NULL,
-    discount INT(10) NOT NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY(customer_id) REFERENCES Customers(id)
+    	id INT(11) NOT NULL AUTO_INCREMENT,
+    	customer_id INT(11) NOT NULL,
+    	required_points INT(11) NOT NULL,
+    	reward_description VARCHAR(255) NOT NULL,
+    	discount INT(10) NOT NULL,
+    	PRIMARY KEY(id),
+    	FOREIGN KEY(customer_id) REFERENCES Customers(id)
 );
 
 CREATE TABLE Billing (
